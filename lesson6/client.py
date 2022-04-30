b753a13743afbc2d8cbf4b904d4772d296b4a8e8
BrianMcLucky <volker415@gmail.com>
2022-04-30 07:27:26 +0300

    lesson6

--- lesson6/client.py
+++ lesson6/client.py
@@ -1,25 +1,27 @@
-from common.utils import parser, get_user_socket, send_data, get_data
+import utils
+
 from common.variables import PRESENCE
 from log import client_log_config
 import logging
 
 logger = logging.getLogger('chat.client')
 
-if __name__ == '__main__':
+
+def main():
     logger.debug('Start...')
     user_name = input('Enter username>>>')
 
-    parser = parser()
+    parser = utils.parser()
     names = parser.parse_args()
 
-    sock = get_user_socket(names.addr, names.port)
+    sock = utils.get_user_socket(names.addr, names.port)
 
     server_addr = sock.getpeername()
     print(f'Connected to {server_addr[0]}:{server_addr[1]}')
 
     PRESENCE['user']['account_name'] = user_name
     try:
-        send_data(sock, PRESENCE)
+        utils.send_data(sock, PRESENCE)
         logger.info(f'Presence send to {server_addr} : {PRESENCE}')
 
     except ConnectionResetError as c:
@@ -29,7 +31,7 @@ if __name__ == '__main__':
 
     while True:
         try:
-            data = get_data(sock)
+            data = utils.get_data(sock)
             logger.info(f'Data received from {server_addr} : {data}')
         except ConnectionResetError as c:
             logger.error(c)
@@ -40,3 +42,7 @@ if __name__ == '__main__':
 
     logger.debug('End...')
     sock.close()
+
+
+if __name__ == '__main__':
+    main()
--- lesson6/common/utils.py
+++ lesson6/common/utils.py
@@ -1,12 +1,30 @@
 import socket
 import json
 import argparse
+import inspect
+from functools import wraps
+import logging
 
 DEFAULT_IP_ADDRESS = '127.0.0.1'
 DEFAULT_PORT = 7777
 CONNECTIONS = 6
 
+server_logger = logging.getLogger('chat.server')
+client_logger = logging.getLogger('chat.client')
 
+
+def log(func):
+    @wraps(func)
+    def call(*args, **kwargs):
+        outer_func = inspect.stack()[1][3]
+        server_logger.debug(f'Function "{func.__name__}" is called into "{outer_func}"')
+        client_logger.debug(f'Function "{func.__name__}" is called into "{outer_func}"')
+        return func(*args, **kwargs)
+
+    return call
+
+
+@log
 def get_serv_socket(addr, port):
     s = socket.socket()
     s.bind((addr, port))
@@ -14,20 +32,24 @@ def get_serv_socket(addr, port):
     return s
 
 
+@log
 def get_user_socket(addr, port):
     s = socket.socket()
     s.connect((addr, port))
     return s
 
 
+@log
 def send_data(sent, data):
     sent.send(json.dumps(data).encode('utf-8'))
 
 
+@log
 def get_data(sender):
     return json.loads(sender.recv(1024).decode('utf-8'))
 
 
+@log
 def parser():
     pars = argparse.ArgumentParser(description='JSON instant messaging')
 
